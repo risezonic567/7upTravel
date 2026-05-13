05b112363b0bc201f7e75f27967481658a91aedf
Sunny9467 <kumarsunny919200@gmail.com>
2026-05-13 11:54:23 +0530

    chnages

--- Backend/controller/PaymentController.js
+++ Backend/controller/PaymentController.js
@@ -278,7 +278,7 @@ export const initiatePayment = async (req, res) => {
 
           order_id: booking._id.toString(),
 
-          currency: "EUR",
+          currency: "USD",
 
           amount: Number(booking.flightData.price),
 
@@ -294,7 +294,7 @@ export const initiatePayment = async (req, res) => {
 
           phone: booking.contact?.phone,
 
-          country: "NL",
+          country: "US",
 
           city: "New York",
 
